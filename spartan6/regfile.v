ba238c02927496806c847fa42ebf65c35b82a514
Arlet Ottens <arlet@c-scape.nl>
2020-10-28 08:50:52 +0100

    Add some comments to regfile

--- spartan6/regfile.v
+++ spartan6/regfile.v
@@ -30,12 +30,14 @@ reg [7:0] regs[15:0];                   // register file
 /*
  * init conversion to 4 banks of 2 bits
  *
+ *                  +--- each column is 4x2 bits for each register
+ *                  v 
  * addr:  A9876543210
  * ---------------------
- * l0     20203123132   10_0010_0011_0110_1101_1110 
- * l1     33203033100   11_1110_0011_0011_1101_0000 
- * h0     33303033000   11_1111_0011_0011_1100_0000
- * h1     33303033100   11_1111_0011_0011_1101_0000
+ * 1:0    20203123132   10_0010_0011_0110_1101_1110 
+ * 3:2    33203033100   11_1110_0011_0011_1101_0000 
+ * 5:4    33303033000   11_1111_0011_0011_1100_0000
+ * 7:6    33303033100   11_1111_0011_0011_1101_0000
  */
 
 wire [4:0] reg_wr = {3'b000, op[5:4]};    // register file select
@@ -44,16 +46,26 @@ wire we = op[6];
 
 /*
  * lower nibble of register file
+ *
+ * The RAM32M primitive is implemented using 4 LUTs in a single slice
+ * 
+ * It has 4 address ports, which can all be used for reading. 
+ * The write address is shared between all 4, and given in ADDRD.
+ * Each port gives access to 2 bits. We can't read from port D, 
+ * which only leaves 6 possible read bits.
+ *
+ * Since we need 8 bits total, we use 2 memories, and only use
+ * 4 bits from each.
  */
 RAM32M #(.INIT_A(64'h2236DE), .INIT_B(64'h3E33DC)) ram_l(
     .ADDRA(reg_rd),
     .ADDRB(reg_rd),
-    .ADDRC(0),
+    .ADDRC(5'b0),
     .ADDRD(reg_wr),
     .DIA(DI[1:0]),
     .DIB(DI[3:2]),
-    .DIC(0),
-    .DID(0),
+    .DIC(2'b0),
+    .DID(2'b0),
     .WCLK(clk),
     .WE(we),
     .DOA(DO[1:0]),
@@ -65,12 +77,12 @@ RAM32M #(.INIT_A(64'h2236DE), .INIT_B(64'h3E33DC)) ram_l(
 RAM32M #(.INIT_A(64'h3F33C0), .INIT_B(64'h3F33D0)) ram_h(
     .ADDRA(reg_rd),
     .ADDRB(reg_rd),
-    .ADDRC(0),
+    .ADDRC(5'b0),
     .ADDRD(reg_wr),
     .DIA(DI[5:4]),
     .DIB(DI[7:6]),
-    .DIC(0),
-    .DID(0),
+    .DIC(2'b0),
+    .DID(2'b0),
     .WCLK(clk),
     .WE(we),
     .DOA(DO[5:4]),
