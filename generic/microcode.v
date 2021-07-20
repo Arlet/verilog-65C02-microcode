2b400b4065cad5cbe692b5f698565975c5084a70
Daniel Serpell <daniel.serpell@gmail.com>
2021-07-19 22:47:12 -0400

    Simplify RESET logic in the microcode.
    Instead of setting the microcode output on reset, mask the address
    and define the reset output as part of the actual microcode.
    
    This simplifies the synthesis and allows the yosys+icd40 tools to
    infer a block-ram for the module.

--- generic/microcode.v
+++ generic/microcode.v
@@ -5,18 +5,14 @@ module microcode(
     input [8:0] addr,
     output reg [31:0] data );
 
-localparam RESET = 32'b000_00000_01_000_0000_00000_00_01110000;
+localparam RESET = 9'b111111111;
 
 reg [31:0] rom[0:511];
 
 initial $readmemb( "microcode.hex", rom, 0 );
-initial data = RESET;
 
 always @(posedge clk)
     if( enable )
-        if( reset )
-            data <= RESET;
-        else
-            data <= rom[addr];
+        data <= rom[reset ? RESET : addr];
 
 endmodule
